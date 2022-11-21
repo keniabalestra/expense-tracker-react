e701f7409c7d827c8a214e14529d7162f6acf937
keniabalestra <kenia.balestra@gmail.com>
2022-11-21 14:24:25 -0500

    Add Card component

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ExpenseItem from './ExpenseItem';
+import Card from './Card';
 import './Expenses.css'
 
 function Expenses(props) {
   return (
-    <div className='expenses'>
+    <Card className='expenses'>
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
@@ -26,7 +27,7 @@ function Expenses(props) {
         date={props.items[3].date}
       />
 
-    </div>
+    </Card>
   );
 }
 
