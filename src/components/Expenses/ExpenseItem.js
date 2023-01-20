9bd7294b29b39665daa2fed921d2c2e517f31f9c
keniabalestra <kenia.balestra@gmail.com>
2023-01-19 21:43:59 -0500

    Add clickHandler

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,10 +1,13 @@
 import React from 'react';
+
 import './ExpenseItem.css';
-import ExpenseDate from './ExpenseDate'
+import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 function ExpenseItem(props) {
+  const clickHandler = () => {
 
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -12,6 +15,7 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change title</button>
     </Card>
   );
 }
